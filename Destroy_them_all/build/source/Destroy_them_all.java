049d38969a78e72849c25a7c8d1cfd1b53dab1cf
deweydbb <deweydbb@gmail.com>
2017-05-02 09:09:55 -0500

    nothing impotant

--- Destroy_them_all/build/source/Destroy_them_all.java
+++ Destroy_them_all/build/source/Destroy_them_all.java
@@ -998,7 +998,7 @@ PShape cloud1, cloud2, cloud3, cloud4, cloud5, cloud6, cloud7, cloud8, cloud9;
 public void loadSprites() {
   building1 = loadImage("Graphics/Buildings/Buildings_Artboard 1.png");
   building2 = loadImage("Graphics/Buildings/Buildings_Artboard 2.png");
-  building3 = loadImage("Graphics/Buildings/Buildings_Artboard 7.png");
+  building3 = loadImage("Graphics/Buildings/Buildings_Artboard 3.png");
   building4 = loadImage("Graphics/Buildings/Buildings_Artboard 4.png");
   building5 = loadImage("Graphics/Buildings/Buildings_Artboard 5.png");
   building6 = loadImage("Graphics/Buildings/Buildings_Artboard 6.png");
